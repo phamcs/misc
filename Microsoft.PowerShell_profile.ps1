b4519cffc6e69cbc623171fc1da1a687250b0b39
Pham Computer Science <phamcs@users.noreply.github.com>
2023-09-19 23:34:36 -0600

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
-# Setting My Documents
+name# Setting My Documents
 $parameters = @{
     Name = "MyDocs"
     PSProvider = "FileSystem"
@@ -14,6 +14,6 @@ Set-Alias -Name python2 -Value "C:\Python27\python.exe"
 Set-Alias -Name pip2 -Value "C:\Python27\Scripts\pip2.exe"
 Set-Alias -Name ruby -Value "C:\Ruby26-x64\bin\ruby.exe"
 Set-Alias -Name vagrant -Value "C:\HashiCorp\Vagrant\bin\vagrant.exe"
-Set-Alias -Name vsm -Value "C:\Users\doanph01\visualvm_206\bin\visualvm.exe"
+Set-Alias -Name vsm -Value "C:\Users\<username>\visualvm_206\bin\visualvm.exe"
 Set-Alias -Name pycharm -Value "C:\Program Files\JetBrains\PyCharm 2020.1.2\bin\pycharm64.exe"
 Set-Alias -Name keytool -Value "C:\Program Files\Java\jdk-11.0.7\bin\keytool.exe"
