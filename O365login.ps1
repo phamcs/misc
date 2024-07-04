80f051b0058f57313c6330834bda2fcc344cbcba
Pham Computer Science <phamcs@users.noreply.github.com>
2024-07-04 12:47:24 -0600

    Update O365login.ps1

--- O365login.ps1
+++ O365login.ps1
@@ -6,5 +6,5 @@ $LiveCred = Get-Credential
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential $LiveCred -Authentication Basic -AllowRedirection
 Import-PSSession $Session
 #Connect-ExchangeOnline -UserPrincipalName <UPN> -ShowProgress $true [-ConnectionUri <URL>] [-AzureADAuthorizationEndPointUri <URL>] [-DelegatedOrganization <String>]
-Connect-ExchangeOnline -UserPrincipalName doanph01@noa.nintendo.com -BypassMailboxAnchoring -ConnectionUri https://outlook.office.com/powershell-liveid/ -AzureADAuthorizationEndPointUri https://noa.awsapps.com/start/ -Credential $livecred
+Connect-ExchangeOnline -UserPrincipalName username@domain -BypassMailboxAnchoring -ConnectionUri https://outlook.office.com/powershell-liveid/ -AzureADAuthorizationEndPointUri https://<oauthapps>/start/ -Credential $livecred
 }
