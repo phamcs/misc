4bed35c40e57fb9b02e8fa5e51c47d18b7ab5757
Pham Computer Science <phamcs@gmail.com>
2025-04-16 08:07:32 -0600

    Update script

--- utils/build_tpl.sh
+++ utils/build_tpl.sh
@@ -3,7 +3,7 @@
 imageURL=https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img
 imageName="jammy-server-cloudimg-amd64.img"
 volumeName="local-zfs"
-virtualMachineId="9000"
+vmid="9000"
 templateName="jammy-tpl"
 tmp_cores="2"
 tmp_memory="2048"
@@ -12,16 +12,17 @@ cpuTypeRequired="kvm64"
 
 apt update && apt install libguestfs-tools -y
 wget -O $imageName $imageURL
-qm destroy $virtualMachineId
-virt-customize -a $imageName --install qemu-guest-agent
+qm destroy $vmid
+virt-customize -a $imageName --install qemu-guest-agent,openssh-server
+virt-customize -a $imageName --run-command 'useradd -m k0ng -p "$rootPasswd"'
 virt-customize -a $imageName --root-password password:$rootPasswd
-qm create $virtualMachineId --name $templateName --memory $tmp_memory --cores $tmp_cores --net0 virtio,bridge=vmbr0
-qm importdisk $virtualMachineId $imageName $volumeName
-qm set $virtualMachineId --scsihw virtio-scsi-pci --virtio0 $volumeName:vm-$virtualMachineId-disk-0
-qm set $virtualMachineId --boot c --bootdisk virtio0
-qm set $virtualMachineId --ide2 $volumeName:cloudinit
-qm set $virtualMachineId --serial0 socket --vga serial0
-qm set $virtualMachineId --ipconfig0 ip=dhcp
-qm set $virtualMachineId --cpu cputype=$cpuTypeRequired
-qm template $virtualMachineId
+qm create $vmid --name $templateName --memory $tmp_memory --cores $tmp_cores --net0 virtio,bridge=vmbr0
+qm importdisk $vmid $imageName $volumeName
+qm set $vmid --scsihw virtio-scsi-pci --virtio0 $volumeName:vm-$vmid-disk-0
+qm set $vmid --boot c --bootdisk virtio0
+qm set $vmid --ide2 $volumeName:cloudinit
+qm set $vmid --serial0 socket --vga serial0
+qm set $vmid --ipconfig0 ip=dhcp
+qm set $vmid --cpu cputype=$cpuTypeRequired
+qm template $vmid
 rm -f $imageName
