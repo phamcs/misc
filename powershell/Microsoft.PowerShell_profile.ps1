458c1de0a0872e74dd5ff18babcf630a24985e10
Pham Computer Science <phamcs@gmail.com>
2025-04-16 20:40:51 -0600

    Update script

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
-name# Setting My Documents
+# Setting My Documents
 $parameters = @{
     PSProvider = "FileSystem"
     Root = "C:\Users\<username>\Documents"
@@ -7,10 +7,5 @@ $parameters = @{
 New-PSDrive @parameters
 # Set Alias
 Set-Alias -Name aws -Value "C:\Program Files\Amazon\AWSCLIV2\aws.exe"
-Set-Alias -Name pip3 -Value "C:\Python38\Scripts\pip3.exe"
-Set-Alias -Name python3 -Value "C:\Python312\python.exe"
-Set-Alias -Name ruby -Value "C:\Ruby26-x64\bin\ruby.exe"
-Set-Alias -Name vagrant -Value "C:\HashiCorp\Vagrant\bin\vagrant.exe"
-Set-Alias -Name vsm -Value "C:\Users\<username>\visualvm_206\bin\visualvm.exe"
 Set-Alias -Name code -Value "C:\Program Files\Microsoft VS Code\Code.exe"
 Set-Alias -Name keytool -Value "C:\Program Files\Java\jdk-11.0.7\bin\keytool.exe"
