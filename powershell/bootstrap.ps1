6e97503f3091bc55beada13b935aaf6cf3f46e73
Pham Computer Science <phamcs@gmai.com>
2025-05-18 01:00:23 -0600

    Update win-install-packages

--- powershell/bootstrap.ps1
+++ powershell/bootstrap.ps1
@@ -11,19 +11,6 @@ $msiArray = @(
     'AWSToolkitForVisualStudio2010-2012_tk-1.10.0.7.msi'
     'AWSToolsAndSDKForNet_sdk-3.7.660.0_ps-4.1.428.0_tk-1.14.5.2.msi'
 )
-$appArray = @(
-    '7zip'
-    'awscli'
-    #'chef-workstation'
-    'golang'
-    'nodejs-lts'
-    'notepadplusplus'
-    'python313'
-    'ruby'
-    #'vagrant'
-    #'virtualbox'
-    'VisualStudioCode'
-)
 $modArray = @(
     'PowerShellGet'
     'PolicyFileEditor'
@@ -44,63 +31,7 @@ Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine -Force
 Set-ItemProperty -Path 'HKLM:\Software\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell' -Name ExecutionPolicy -Value Unrestricted
 Get-ExecutionPolicy -List | Format-Table -hideTableHeader
 gpupdate /force
-# Setup WinRM
-winrm quickconfig -q
-winrm set winrm/config/client/auth '@{Basic="true"}'
-winrm set winrm/config/client '@{AllowUnencrypted="True"}'
-winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="512"}'
-winrm set winrm/config '@{MaxTimeoutms="1800000"}'
-# netsh advfirewall firewall add rule name="WinRM-HTTP" dir=in localport=5985 protocol=TCP action=allow
-# Enables the WinRM service and sets up the HTTP listener
-Enable-PSRemoting -Force
 
-# Opens port 5985-5986 for all profiles
-$firewallParams = @{
-    Action      = 'Allow'
-    Description = 'Inbound rule for WinRM via WS-Management. [TCP 5985-5986]'
-    Direction   = 'Inbound'
-    DisplayName = 'WinRM (HTTP/HTTPS-In)'
-    LocalPort   = 5985-5986
-    Profile     = 'Any'
-    Protocol    = 'TCP'
-}
-New-NetFirewallRule @firewallParams
-
-# Allows local user accounts to be used with WinRM
-# This can be ignored if using domain accounts
-$tokenFilterParams = @{
-    Path         = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System'
-    Name         = 'LocalAccountTokenFilterPolicy'
-    Value        = 1
-    PropertyType = 'DWORD'
-    Force        = $true
-}
-New-ItemProperty @tokenFilterParams
-
-# Create self signed certificate
-$certParams = @{
-    CertStoreLocation = 'Cert:\LocalMachine\My'
-    DnsName           = $env:COMPUTERNAME
-    NotAfter          = (Get-Date).AddYears(1)
-    Provider          = 'Microsoft Software Key Storage Provider'
-    Subject           = "CN=$env:COMPUTERNAME"
-}
-$cert = New-SelfSignedCertificate @certParams
-
-# Create HTTPS listener
-$httpsParams = @{
-    ResourceURI = 'winrm/config/listener'
-    SelectorSet = @{
-        Transport = "HTTPS"
-        Address   = "*"
-    }
-    ValueSet = @{
-        CertificateThumbprint = $cert.Thumbprint
-        Enabled               = $true
-    }
-}
-New-WSManInstance @httpsParams
-Set-Service -Name WinRM -Status Running -PassThru
 # Setup SSH
 Get-WindowsCapability -Name OpenSSH.Server* -Online | Add-WindowsCapability -Online
 Set-Service -Name sshd -StartupType Automatic -Status Running
@@ -131,16 +62,12 @@ foreach ($msi in $msiArray) {
 # Install chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco upgrade -y chocolatey
-foreach ($app in $appArray) {
-  choco install -y $app --force
-} 
-# Adding new Path to ENV
-Set-Item -Path Env:Path -Value ($Env:Path + "C:\Python313;C:\tools\ruby34\bin")
+
 # Setup WSL
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
 wsl --set-default-version 2; wsl --update;
-# wsl --install -d Ubuntu-22.04 -n; wsl -s Ubuntu-22.04
+
 # Reboot your computer to apply changes
 $reboot = Read-Host "Do you want to reboot the computer? (y/n)"
 if ($reboot -eq "y") {
