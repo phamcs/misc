dfc680e538653877173ef1a022bea7395123a26c
phamcs <phamcs@gmail.com>
2025-09-18 21:48:35 -0600

    Update win install/uninstall scripts

--- powershell/install-apps.ps1
+++ powershell/install-apps.ps1
@@ -20,7 +20,9 @@ $appArray = @(
     'vscode'
 )
 # Install chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+PSScriptAnalyzer(PSAvoidUsingCmdletAliases) ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco upgrade -y chocolatey
 foreach ($app in $appArray) {
   choco install -y $app --force
