f2deac827193a0dea63e7b1d38ac0432b3438b0c
Pham Computer Science <phamcs@gmail.com>
2026-07-19 11:01:55 -0600

    Update manifests

--- powershell/O365login.ps1
+++ powershell/O365login.ps1
@@ -8,3 +8,16 @@ Import-PSSession $Session
 #Connect-ExchangeOnline -UserPrincipalName <UPN> -ShowProgress $true [-ConnectionUri <URL>] [-AzureADAuthorizationEndPointUri <URL>] [-DelegatedOrganization <String>]
 Connect-ExchangeOnline -UserPrincipalName username@domain -BypassMailboxAnchoring -ConnectionUri https://outlook.office.com/powershell-liveid/ -AzureADAuthorizationEndPointUri https://<oauthapps>/start/ -Credential $livecred
 }
+
+curl --location --request POST 'https://dragonfly.superasian.net/oapi/v1/jobs' \
+  --header 'Content-Type: application/json' \
+  --header 'Authorization: Bearer OTY2MjZjOWQtMmZiZi00MWYzLWJkZmMtZTQwYjg2MmNlYWZi' \
+  --data-raw '{
+  "type": "preheat",
+  "args": {
+    "type": "image",
+    "url": "https://index.docker.io/v2/library/alpine/manifests/3.19",
+    "scope": "single_seed_peer"
+  },
+  "scheduler_cluster_ids":[1]
+}'
